a00ff26f07b45eab20084df9c7855f9eba846524
Ravinder Singh <ravindermaan0123@gmail.com>
2022-01-05 13:25:10 +0530

    created home page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hi there
-      </header>
+      <Home />
     </div>
   );
 }
